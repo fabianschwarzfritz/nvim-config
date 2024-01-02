5a39938ce3ae7ed5f7a943d1fad01582c32e9e8f
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2024-01-02 15:07:30 +0100

    MacOS seems to have <C-j> already in use

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -11,8 +11,8 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ["<C-Space>"] = cmp.mapping.complete(),
 })
 
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
+--cmp_mappings['<Tab>'] = nil
+--cmp_mappings['<S-Tab>'] = nil
 
 --lsp.setup_nvim_cmp({
 --  mapping = cmp_mappings
@@ -40,7 +40,7 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("i", "<C-d>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
