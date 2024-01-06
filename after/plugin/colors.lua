e47a81181b4cfc93f3db20c5db41649a312ce5df
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2024-01-06 16:37:28 +0100

    Light alternative :)

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,7 @@
 function SetColorScheme(color)
 	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+    vim.cmd.colorscheme(color)
+    vim.o.background = "light"
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
